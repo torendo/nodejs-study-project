a491eb4da117fad45ea25fe0b82278c1f9764a67
nekro20 <toren4eg@gmail.com>
2017-09-26 00:36:55 +0300

    HW2 finished

--- dirwatcher/index.js
+++ dirwatcher/index.js
@@ -12,6 +12,7 @@ export default class DirWatcher extends EventEmitter {
         }
         oldFiles = files;
       });
-    }, delay)
+    }, delay);
+    return this;
   }
 };
--- importer/index.js
+++ importer/index.js
@@ -1,42 +1,42 @@
-//It should be able to listen to DirWatcher events and start import csv files on ‘dirwatcher:changed’ event.
 import fs from 'fs';
 import util from 'util';
+import csv from 'csvjson';
 import DirWatcher from '../dirwatcher';
 
 const readFileAsync = util.promisify(fs.readFile);
 
 export default class Importer {
   constructor(path, delay) {
-    const dirWatcher = new DirWatcher();
-    dirWatcher.watch(path, delay);
-    dirWatcher.on('changed', (files) => {
-      new Promise.all(files.map(file => this.importAsync(file))).then((data) => {
-        console.log(data)
-      })
-    });
-    dirWatcher.on('changed', (files) => {
-      files.forEach((file) => {
-        let data = this.importSync(file);
-        console.log(data);
-      })
-    });
+    this.path = path;
+    this.dirWatcher = new DirWatcher();
+    this.dirWatcher
+      .watch(path, delay)
+      .on('changed', files => {
+        this.importAsync(files).then(data => {
+          console.log('importAsync:\n', data);
+        });
+      }).on('changed', files => {
+        let data = this.importSync(files);
+        console.log('importSync:\n', data);
+      });
   }
-  importAsync(path) {
-    //should be able to return promise with imported data
-    return async (file) => {
+  importAsync(files) {
+    return Promise.all(files.map(async file => {
+      let content;
       try {
-        await readFileAsync(path + '/' + file);
+        content = await readFileAsync(this.path + '/' + file, 'utf8');
       } catch (err) {
         throw err;
       }
-    };
+      return csv.toObject(content);
+    }));
   }
-  importSync(path) {
-    fs.readFileSync(path, (err, data) => {
-      if (err) throw err;
-      return data;
+  importSync(files) {
+    return files.map(file => {
+      let content = fs.readFileSync(this.path + '/' + file, 'utf8', err => {
+        if (err) throw err;
+      });
+      return csv.toObject(content);
     });
-    //is synchronous and should be able to return all imported data
   }
-  //All imported data should have a json format
 };
